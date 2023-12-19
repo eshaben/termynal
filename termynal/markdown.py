4e6e41ad5cd6266b3663dade7044a25e4e96eb93
Erin Shaben <eshaben@icloud.com>
2023-12-18 21:36:27 -0500

    test

--- termynal/markdown.py
+++ termynal/markdown.py
@@ -175,7 +175,7 @@ class Termynal:
 class TermynalPreprocessor(Preprocessor):
     """Converts fenced code blocks to termynal HTML."""
 
-    ty_comment = re.compile(r"<!--\s*\*?termynal\*?-->(.*?)```", re.DOTALL)
+    ty_comment = re.compile(r"^\s*<!--\s*termynal:?(.*)-->", re.MULTILINE)
     marker = "9HDrdgVBNLga"
     FENCED_BLOCK_RE = re.compile(
         dedent(
@@ -196,6 +196,7 @@ class TermynalPreprocessor(Preprocessor):
         ),
         re.MULTILINE | re.DOTALL | re.VERBOSE,
     )
+
 
     def __init__(self, config: Config, md: "core.Markdown"):
         super(TermynalPreprocessor, self).__init__(md=md)
